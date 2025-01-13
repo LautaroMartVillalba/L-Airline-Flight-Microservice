668f43fb4fde7587192e4fa5577b0f98536baea9
Lautaro Villalba <lautaromartinvillalba@gmail.com>
2025-01-12 22:40:40 -0300

    REFACTOR: clean code

--- src/main/java/ar/com/l_airline/LAirlineApplication.java
+++ src/main/java/ar/com/l_airline/LAirlineApplication.java
@@ -1,24 +1,13 @@
 package ar.com.l_airline;
 
-import ar.com.l_airline.services.FlightService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LAirlineApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LAirlineApplication.class, args);
-	}
-
-	@Bean
-	public CommandLineRunner run(FlightService flightService){
-
-		return args -> {
-		};
-
 	}
 
 }
