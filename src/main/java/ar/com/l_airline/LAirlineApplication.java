d99da7fa0e936084b077ecd305f51dcad4ab50d2
Lautaro Villalba <lautaromartinvillalba@gmail.com>
2025-03-05 19:11:36 -0300

    REFACTOR: Changes to debug testing

--- src/main/java/ar/com/l_airline/LAirlineApplication.java
+++ src/main/java/ar/com/l_airline/LAirlineApplication.java
@@ -1,13 +1,30 @@
 package ar.com.l_airline;
 
+import ar.com.l_airline.services.AirplaneService;
+import ar.com.l_airline.services.AirportService;
+import ar.com.l_airline.services.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class LAirlineApplication {
+@EnableDiscoveryClient
+public class LAirlineApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LAirlineApplication.class, args);
 	}
+	@Autowired
+	TicketService ticketService;
+	@Autowired
+	AirportService airportService;
+	@Autowired
+	AirplaneService airplaneService;
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
